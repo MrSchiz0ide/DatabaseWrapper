96a5a0beac8738db7f20b39dc510817d4f77558d
MrSchiz0ide <re_player09@hotmail.com>
2013-09-05 16:10:51 +0200

    Create config.php

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+Config::Write('DB_HOST', 'enter host here');
+Config::Write('DB_NAME', 'enter database name here');
+Config::Write('DB_USER', 'enter database username here');
+Config::Write('DB_PASS', 'enter database password here');
+?>
