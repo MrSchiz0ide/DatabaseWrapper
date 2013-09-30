1a9e78fe053dcf780a2ee5a96585169f591891a8
MrSchiz0ide <asylumproductionshd@gmail.com>
2013-09-30 15:35:17 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php
-require_once("/Controller/DBFactory.php");
-require_once("/Controller/DBHandler.php");
 require_once("config.php")
 
-//example usage: DBFactoryChild::Function(parameters);
+//do something here
 ?>
