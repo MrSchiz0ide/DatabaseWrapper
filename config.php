bba34470bf0b8cbffe9bb8b007408e5115d79098
MrSchiz0ide <asylumproductionshd@gmail.com>
2013-09-30 15:35:48 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,4 +1,9 @@
 <?php
+/* Includes */
+require_once("/Controller/DBFactory.php");
+require_once("/Controller/DBHandler.php");
+
+/* Dynamic Configuration */
 Config::Write('DB_HOST', 'enter host here');
 Config::Write('DB_NAME', 'enter database name here');
 Config::Write('DB_USER', 'enter database username here');
