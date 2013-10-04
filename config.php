32b11d4eba9d8f3f316c6be1dea36fa7d7149499
MrSchiz0ide <asylumproductionshd@gmail.com>
2013-10-04 23:30:54 +0200

    Update config.php

--- config.php
+++ config.php
@@ -4,8 +4,8 @@ require_once("/Controller/DBFactory.php");
 require_once("/Controller/DBHandler.php");
 
 /* Dynamic Configuration */
-Config::Write('DB_HOST', 'enter host here');
-Config::Write('DB_NAME', 'enter database name here');
-Config::Write('DB_USER', 'enter database username here');
-Config::Write('DB_PASS', 'enter database password here');
+Config\handler::Write('DB_HOST', 'enter host here');
+Config\handler::Write('DB_NAME', 'enter database name here');
+Config\handler::('DB_USER', 'enter database username here');
+Config\handler::Write('DB_PASS', 'enter database password here');
 ?>
